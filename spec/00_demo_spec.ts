a640febf6786633aefc99d4f374e14dfc8427da1
hilarydsouza <hd_souza@hotmail.com>
2019-03-14 18:48:14 +0000

    3rd day

---
+++ spec/00_demo_spec.ts
@@ -0,0 +1,14 @@
+describe('Writing a spec', () => {
+    it('is easy when you know how!', () => {
+
+        //Given - Arrange
+        const a = 10, b = 20;
+
+        //When - Act
+        const answer = a + b;
+
+        //Then - Assert
+        expect(answer).toBe(30);
+
+    });
+});
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import './styles.css';
+import { getRandomInt } from './utils';
+
+let guessCount = 0;
+const squares = document.querySelectorAll('.square');
+const count = document.getElementById('count') as HTMLSpanElement;
+const secret = getRandomInt(1, 6);
+
+squares.forEach((sq, idx) => {
+    const el = sq as HTMLDivElement;
+    if ((idx + 1) === secret) {
+        el.dataset.secret = 'true';
+    }
+    sq.addEventListener('click', handleClick)
+});
+function handleClick() {
+    const el = this as HTMLDivElement;
+    guessCount++;
+    count.innerHTML = `<small> You have made <b>${guessCount}</b> guesses </small>`;
+    if (el.dataset.secret) {
+        el.classList.add('winner');
+        squares.forEach(sq => sq.removeEventListener('click', handleClick));
+    } else {
+        el.classList.add('loser');
+        el.removeEventListener('click', handleClick);
+    }
+
+};
